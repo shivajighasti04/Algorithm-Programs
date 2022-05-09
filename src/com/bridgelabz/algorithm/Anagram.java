abe4d04a8c86aa52f3060ef66deba2a80b2a78e2
shivajighasti04 <shivajighasti97@gamil.com>
2022-05-09 17:22:14 +0530

    program to check string is an anagram of another string

---
+++ src/com/bridgelabz/algorithm/Anagram.java
@@ -0,0 +1,57 @@
+package com.bridgelabz.algorithm;
+import java.util.Arrays;
+import java.util.Scanner;
+/**
+ * 
+ * @author Shivaji
+ *  This method checks if two string are anagram.
+ *
+ */
+
+
+public class Anagram {
+public boolean checkIfAnagrams(String firstWord, String secondWord) {
+		
+		firstWord = firstWord.toLowerCase();
+		secondWord = secondWord.toLowerCase();
+		char[] firstWordArray = firstWord.toCharArray();
+		char[] secondWordArray = secondWord.toCharArray();
+		
+		Arrays.sort(firstWordArray);
+		Arrays.sort(secondWordArray);
+		int compareResult = Arrays.compare(firstWordArray, secondWordArray);
+		
+		if(compareResult == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		String firstWord, secondWord;
+		Scanner scannerObject = new Scanner(System.in);
+		Anagram anagramObject = new Anagram();
+		
+		System.out.println("Enter the first word : ");
+		firstWord = scannerObject.next();
+		System.out.println("Enter the second word : ");
+		secondWord = scannerObject.next();
+		scannerObject.close();
+		
+		boolean result = anagramObject.checkIfAnagrams(firstWord, secondWord);
+		
+		if(result)
+			System.out.println("The words are anagrams");
+		else
+			System.out.println("The words are not anagrams");
+
+	}
+
+	
+
+}
+
+
